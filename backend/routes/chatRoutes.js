3cb835f6f418f955076c20ae0eb2b18c51e61965
omkar shelke <omkar1144@gmail.com>
2023-08-12 21:30:40 +0400

    chat backend api contructed

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,14 +2,19 @@ const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 const { chats } = require('../data/data');
 const { route } = require('./useRoutes');
-const {accessChat} = require("../controller/chatController");
+const {accessChat} = require("../controller/chatController")
+const {fetchChats} = require("../controller/chatController");
+const {createGroupChat} = require("../controller/chatController");
+const {renameGroup} = require("../controller/chatController");
+const{addToGroup} = require("../controller/chatController");
+const {removeFromGroup} = require("../controller/chatController");
 const router = express.Router();
 
 router.route('/').post(protect,accessChat);
-// router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, renameFromGroup);
-// router.route("/groupadd").put(protect,addtoGroup);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect,addToGroup);
 
 module.exports = router;
